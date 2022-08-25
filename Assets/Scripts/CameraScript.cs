7c88f9f6b4c7465b82e031e5370efcc85fe0cba3
Gonçalo Oliveira <goncalo.27@hotmail.com>
2022-08-25 22:52:13 +0100

    Visual & Content Update
    Implemented 2 new flying enemies (laser enemy and ball enemy);
    Implemented a cell shader;
    Implemented a grass painter tool;
    Implemented a grass shader with interactive properties with the player and pets;
    Implemented a dash and a jump for the player;
    Added new assets for the levels (trees, rocks and mushrooms).

---
+++ Assets/Scripts/CameraScript.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraScript : MonoBehaviour
+{
+    //Variáveis
+    public Transform player;
+    public float smooth = 0.3f;
+
+    public float height;
+
+    private Vector3 velocity = Vector3.zero;
+
+    //Métodos
+    void Update()
+    {
+        Vector3 pos = new Vector3();
+        pos.x = player.position.x;
+        pos.z = player.position.z - 14f;
+        pos.y = player.position.y + height;
+        transform.position = Vector3.SmoothDamp(transform.position, pos, ref velocity, smooth);
+    }
+}
